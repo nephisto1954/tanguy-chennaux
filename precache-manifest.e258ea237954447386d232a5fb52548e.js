1eff70069d838eb457aab39b1827e3feb25bc372
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-09 07:35:37 +0000

    Deploy nephisto1954/tanguy-chennaux to github.com/nephisto1954/tanguy-chennaux.git:gh-pages

---
+++ precache-manifest.e258ea237954447386d232a5fb52548e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5232dd2cde9661a5ead9e5cdba4ec0c1",
+    "url": "/tanguy-chennaux/index.html"
+  },
+  {
+    "revision": "57f6bff3ef51be5fb956",
+    "url": "/tanguy-chennaux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8ec1d0090849fe25688",
+    "url": "/tanguy-chennaux/static/js/2.f6a02a98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tanguy-chennaux/static/js/2.f6a02a98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f6bff3ef51be5fb956",
+    "url": "/tanguy-chennaux/static/js/main.53af5e8d.chunk.js"
+  },
+  {
+    "revision": "613a7daabaf6dddee92d",
+    "url": "/tanguy-chennaux/static/js/runtime-main.0db9a847.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tanguy-chennaux/static/media/logo.5d5d9eef.svg"
+  }
+]);
